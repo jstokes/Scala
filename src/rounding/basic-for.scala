cbbd63bc265b71d3a5b886b045b74499f392c762
Jeff <stokesj07@students.ecu.edu>
2010-12-10 13:27:35 -0500

    added basic-for files.

---
+++ src/rounding/basic-for.scala
@@ -0,0 +1,5 @@
+val dogBreeds = List("Doberman", "Yorkshire Terrier", "Dachshund",
+                    "Scottish Terrier", "Great Dane", "Portuguese Water Dog")
+
+for (breed <- dogBreeds)
+  println(breed)
